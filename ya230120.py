20baae9dc5abf6c0c13e291be1242411c40fce81
july-ya <123149377+july-ya@users.noreply.github.com>
2023-01-26 17:03:26 +0900

    Update ya230120.py
    [23.01.26]
    1) def plotting_demo(): 함수 정의
    2) sidebar에 radio버튼으로 원하는 차트 선택(현재 line만 됨)

--- ya230120.py
+++ ya230120.py
@@ -3,40 +3,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-money = pd.read_csv("money_data7.csv")
+#money = pd.read_csv("money_data7.csv")
 
 st.title(':polar_bear: This is :orange[july-ya] page :polar_bear:')
-st.sidebar.success("Select a demo above.")
+#st.sidebar.success("Select a demo above.")
 
-option = st.selectbox('How would you like to choice year ?', ('2020', '2021', '2022'))
-option2 = int(option)
-st.write('You selected:', option)
+def plotting_demo():
+  money = pd.read_csv("money_data7.csv")
+  option = st.selectbox('How would you like to choice year ?', ('2020', '2021', '2022'))
+  option2 = int(option)
+  st.write('You selected:', option)
 
-money = money[:] [money['A_YEAR']== option2]
+  money = money[:] [money['A_YEAR']== option2]
 
+  fig, ax = plt.subplots(2,2, figsize=(12,8))
 
-fig, ax = plt.subplots(2,2, figsize=(12,8))
+  plt.subplot(221)
+  plt.plot( list(money['A_MONTH']), list(money['A_RATE']), color = 'salmon', marker = 'o' )
+  plt.xticks(tuple(money['A_MONTH']))
+  plt.title("America Rate", size = 15, color = 'salmon')
 
-plt.subplot(221)
-plt.plot( list(money['A_MONTH']), list(money['A_RATE']), color = 'salmon', marker = 'o' )
-plt.xticks(tuple(money['A_MONTH']))
-plt.title("America Rate", size = 15, color = 'salmon')
+  plt.subplot(222)
+  plt.plot( list(money['A_MONTH']), list(money['K_RATE']), color = 'lightskyblue', marker = 'o' )
+  plt.xticks(tuple(money['A_MONTH']))
+  plt.title("Korea Rate", size = 15, color = 'lightskyblue')
 
-plt.subplot(222)
-plt.plot( list(money['A_MONTH']), list(money['K_RATE']), color = 'lightskyblue', marker = 'o' )
-plt.xticks(tuple(money['A_MONTH']))
-plt.title("Korea Rate", size = 15, color = 'lightskyblue')
+  plt.subplot(223)
+  plt.plot( list(money['A_MONTH']), list(money['KOSPI']), color = 'lightgreen', marker = 'o' )
+  plt.xticks(tuple(money['A_MONTH']))
+  plt.title("KOSPI", size = 15, color = 'lightgreen')
 
-plt.subplot(223)
-plt.plot( list(money['A_MONTH']), list(money['KOSPI']), color = 'lightgreen', marker = 'o' )
-plt.xticks(tuple(money['A_MONTH']))
-plt.title("KOSPI", size = 15, color = 'lightgreen')
+  plt.subplot(224)
+  plt.plot( list(money['A_MONTH']), list(money['HOUSE_PRICE']), color = 'lightpink', marker = 'o' )
+  plt.xticks(tuple(money['A_MONTH']))
+  plt.title("House Price", size = 15, color = 'lightpink')
 
-plt.subplot(224)
-plt.plot( list(money['A_MONTH']), list(money['HOUSE_PRICE']), color = 'lightpink', marker = 'o' )
-plt.xticks(tuple(money['A_MONTH']))
-plt.title("House Price", size = 15, color = 'lightpink')
+  st.snow()
+  st.pyplot(fig)
+  st.dataframe(money)
 
-st.snow()
-st.pyplot(fig)
-st.dataframe(money)
+with st.form(key = 'Form1'):
+  with st.sidebar:
+    select_chart = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie'))
+
+if select_chart == 'line':
+  plotting_demo()
